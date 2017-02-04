58441d4383dbbbb499ce9c3cb894699f94476077
Oguz Meteer <info@guztech.nl>
2017-02-04 17:55:33 +0100

    Handle case where push and pop are asserted
    Add a comment header explaining the parameters and
    what happens if both push and pop are asserted
    simultaneously.
    
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- stack.v
+++ stack.v
@@ -1,4 +1,15 @@
-`timescale 1ns / 1ps
+/*
+A simple circular stack (LIFO).
+
+STACK_WIDTH - The bit width of the words on the stack.
+STACK_SIZE  - Size of the stack (2 ^ STACK_SIZE words).
+
+If push and pop are asserted at the same time, the old
+TOS (top of stack) value will appear at data_out and
+the value of data_in will be written to the same
+position. Just asserting pop would then place the newly
+written data on the data_out port. 
+*/
 
 module stack #(
     parameter STACK_WIDTH = 18,
@@ -21,14 +32,15 @@ module stack #(
             stack_ptr <= 0;
             data_out  <= 0;
         end else begin
-            if (push) begin
+            if (push && !pop) begin
                 mem[stack_ptr] <= data_in;
                 stack_ptr      <= stack_ptr + 1'b1;
-            end
-            
-            if (pop) begin
+            end else if (pop && !push) begin
                 data_out  <= mem[ptr_m];
                 stack_ptr <= stack_ptr - 1'b1;
+            end else if (push && pop) begin
+                data_out   <= mem[ptr_m];
+                mem[ptr_m] <= data_in;
             end
         end 
     end
