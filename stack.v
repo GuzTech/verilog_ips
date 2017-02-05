d11a676c59d92179904aece9e1eda695b94a4ba1
Oguz Meteer <info@guztech.nl>
2017-02-05 03:01:29 +0100

    Refactor code so that block RAM is inferred
    By refactoring the code, read-first dual-ported block RAM is inferred.
    
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- stack.v
+++ stack.v
@@ -13,7 +13,7 @@ written data on the data_out port.
 
 module stack #(
     parameter STACK_WIDTH = 18,
-    parameter STACK_SIZE  = 4
+    parameter STACK_SIZE  = 1
 ) (
     input                        clk,
     input                        reset,
@@ -22,7 +22,6 @@ module stack #(
     input      [STACK_WIDTH-1:0] data_in,
     output reg [STACK_WIDTH-1:0] data_out
 );
-
     reg [STACK_WIDTH-1:0] mem[0:2**STACK_SIZE-1];
     reg [ STACK_SIZE-1:0] stack_ptr;
     reg [ STACK_SIZE-1:0] ptr_m;
@@ -32,27 +31,26 @@ module stack #(
             stack_ptr <= 0;
             data_out  <= 0;
         end else begin
-            if (push && !pop) begin
-                mem[stack_ptr] <= data_in;
-                stack_ptr      <= stack_ptr + 1'b1;
-            end else if (pop && !push) begin
+            if (push) begin                
+                if (!pop) begin // Just push
+                    mem[stack_ptr] <= data_in;
+                    stack_ptr      <= stack_ptr + 1'b1;
+                end else begin // Push and pop
+                    data_out   <= mem[ptr_m];
+                    mem[ptr_m] <= data_in;
+                end
+            end else if (pop) begin // Just pop
                 data_out  <= mem[ptr_m];
-                stack_ptr <= stack_ptr - 1'b1;
-            end else if (push && pop) begin
-                data_out   <= mem[ptr_m];
-                mem[ptr_m] <= data_in;
+                stack_ptr <= ptr_m;
             end
         end 
     end
-    
+
     always @ (*) begin
-        // Use 1'b1 and not 1, because 1 is
-        // an integer and by default, they
-        // are 32 bits. Verilog arithmetic
-        // uses the bit width of the largest
-        // opererand for the entire expression,
-        // meaning you'll get a truncation
-        // warning.
+        // Use 1'b1 and not 1, because 1 is an integer and by default,
+        // they are 32 bits. Verilog arithmetic uses the bit width of
+        // the largest opererand for the entire expression, meaning 
+        // you'll get a truncation warning.
         ptr_m = stack_ptr - 1'b1;
     end
 endmodule
