98b777e51c04b86c79d6b78edec464f271d1be43
Oguz Meteer <info@guztech.nl>
2019-02-13 11:41:13 +0100

    Release all code under ISC license

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# verilog_ips
+## verilog_ips
 Various IPs implemented in Verilog
 
 * stack.v - A circular stack
@@ -6,5 +6,11 @@ Various IPs implemented in Verilog
 * deserializer_rst.sv - With a reset signal.
 * serializer.sv - A formally verified, parameterizable serializer.
 
-# Formal verification
+## Formal verification
 The deserializer, deserializer_rst, and serializer IPs come with formal specifications. You need to install [SymbiYosys](https://symbiyosys.readthedocs.io/en/latest/quickstart.html) first. To check a specific IP, run `make <IP name>` or if you want to check all of them, run `make formal`.
+
+## Status
+The stack implementation has not been formally verified yet, but will be.
+
+## License
+verilog_ips is free and open hardware and is licensed under the [ISC licence](http://en.wikipedia.org/wiki/ISC_license).
--- stack.v
+++ stack.v
@@ -1,15 +1,33 @@
+// verilog_ips: stack
+//
+// Copyright (C) 2019 Oguz Meteer <info@guztech.nl>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 /*
-A simple circular stack (LIFO).
+ * A simple circular stack (LIFO).
+ *
+ * STACK_WIDTH - The bit width of the words on the stack.
+ * STACK_SIZE  - Size of the stack (2 ^ STACK_SIZE words).
+ *
+ * If push and pop are asserted at the same time, the old
+ * TOS (top of stack) value will appear at data_out and
+ * the value of data_in will be written to the same
+ * position. Just asserting pop would then place the newly
+ * written data on the data_out port. 
+ */
 
-STACK_WIDTH - The bit width of the words on the stack.
-STACK_SIZE  - Size of the stack (2 ^ STACK_SIZE words).
-
-If push and pop are asserted at the same time, the old
-TOS (top of stack) value will appear at data_out and
-the value of data_in will be written to the same
-position. Just asserting pop would then place the newly
-written data on the data_out port. 
-*/
+`default_nettype none
 
 module stack #(
     parameter STACK_WIDTH = 18,
